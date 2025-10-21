4d4a7be652194734e4b5ab41cc72bd50601c549e
millitradez <Tradezconsulting@icloud.com>
2025-10-21 13:48:51 -0700

    upload templates and app.py

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/wallet')
+def wallet():
+    return render_template('wallet.html')
+
+@app.route('/trade')
+def trade():
+    return render_template('trade.html')
+
+@app.route('/logs')
+def logs():
+    return render_template('logs.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Millitradez is running successfully!"
+        return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))

2faf81310965b02962ac80fdabacb12f9b168c4a
millitradez <Tradezconsulting@icloud.com>
2025-10-21 19:03:36 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render _template
-app= Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+@app.route('/wallet')
+def wallet():
     return render_template('wallet.html')
-    @app.route('/trade')
-    def trade():
-        return render_templates('trade.html')
-        if __name__ '__main__':
-        app.run(debug=True)
+
+@app.route('/trade')
+def trade():
+    return render_template('trade.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

b28f389262917c7d62731c4c7729cf5a2645a3f7
millitradez <Tradezconsulting@icloud.com>
2025-10-21 18:48:09 -0700

    Update app.py
    updated app.py with correct Flask routes

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from app import app
-import os
-
-if __name__=="__main__":
-    port+ int(os.environ.get("PORT", 5000))
-    app.run(host+0.0.0.0", port+port)
+from flask import Flask, render _template
+app= Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('wallet.html')
+    @app.route('/trade')
+    def trade():
+        return render_templates('trade.html')
+        if __name__ '__main__':
+        app.run(debug=True)

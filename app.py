017ddfd8ecf6821de19c581b0caac096bda2ae7e
millitradez <Tradezconsulting@icloud.com>
2025-10-21 18:03:42 -0700

    Fix render port issue

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from app import app
+import os
+
 if __name__=="__main__":
-    import os
-    port= int(os.environ.get(PORT", 5000))
-    run(host="0.0.0.0", port=por)
+    port+ int(os.environ.get("PORT", 5000))
+    app.run(host+0.0.0.0", port+port)
